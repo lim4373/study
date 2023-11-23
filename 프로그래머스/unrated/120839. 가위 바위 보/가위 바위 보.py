d8c96417d29afe82658a941c816470ef448c17dc
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2023-11-23 23:50:38 +0900

    [level unrated] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.py
@@ -1,6 +1,6 @@
 def solution(rsp):
-    game = {'2':'0', '0':'5','5':'2'}
-    answer = ''
+    my_dict = {"2":"0", "0": "5", "5": "2"}
+    answer =""
     for i in rsp:
-        answer += game.get(i)
+        answer += my_dict.get(i)
     return answer
