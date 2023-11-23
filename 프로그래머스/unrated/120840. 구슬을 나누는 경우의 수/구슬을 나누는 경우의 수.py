247b59f186c89ec61c71f6aba55374e62ffda5b6
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2023-11-23 20:03:30 +0900

    [level unrated] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/unrated/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,3 +1,15 @@
-import math
-def solution(balls, share):
-    return math.comb(balls, share)
+def fac(num):
+    a=1
+    for i in range(1,num+1):
+        a *=i
+    return a
+
+def solution(balls,share):
+    answer = 0
+    n = fac(balls)
+    m = fac(share)
+    nm = fac(balls-share)
+
+    answer = n/(nm*m)
+
+    return answer
