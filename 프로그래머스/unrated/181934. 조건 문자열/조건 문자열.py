d82f985a3f4c3330d0924991713ce4f878f3c99b
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2023-11-29 03:14:57 +0900

    [level unrated] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,11 @@
+def solution(ineq, eq, n, m):
+    if eq == "!" :
+        if ineq == "<" :
+             return int(n<m)
+        else:
+            return int(n>m)
+    else:
+        if ineq == "<" :
+             return int(n<=m)
+        else:
+            return int(n>=m)
