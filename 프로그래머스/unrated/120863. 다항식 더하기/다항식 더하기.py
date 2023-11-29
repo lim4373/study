8f0c68f4b4ba3c7c435c1f444ce2e6810445f65f
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2023-11-29 19:23:13 +0900

    [level unrated] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,25 @@
+def solution(polynomial):
+    polynomial = polynomial.replace(' ', '').split('+')
+    
+    a, b = 0, 0
+    for i in polynomial:
+        if 'x' in i:
+            if len(i) > 1:
+                a += int(i[:-1])
+            else:
+                a +=1
+        else:
+            b += int(i)
+    
+    if a == 0:
+        return '{}'.format(b)
+    elif a == 1:
+        if b == 0:
+            return 'x'
+        elif b != 0:
+            return 'x + {}'.format(b)
+    elif a > 1:
+        if b == 0:
+            return '{}x'.format(a)
+        elif b != 0:
+            return '{}x + {}'.format(a, b)
