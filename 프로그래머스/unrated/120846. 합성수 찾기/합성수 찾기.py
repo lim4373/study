c28ce49cb25712b94e10d4f7e9af3950a814a194
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2023-11-30 21:11:11 +0900

    [level unrated] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.py
@@ -1,10 +1,10 @@
 def solution(n):
     num = []
-    answer = 0
-    for i in range(2,n+1):
+    a = 0
+    for i in range(1,n+1):
         for j in range(1,i+1):
             if i%j == 0:
                 num.append(i)
         if num.count(i)>= 3:
-            answer += 1    
-    return answer
+            a += 1
+    return a
