3423d3b4c83aa74afb35ff4edfa0fdbf4ec93e12
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2023-12-13 00:23:51 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,5 @@
+def solution(s):
+    kakao = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    for key in kakao:
+        s= s.replace(key,kakao[key])
+    return int(s)
