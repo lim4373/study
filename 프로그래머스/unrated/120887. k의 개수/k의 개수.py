7d71786ebf912a68efe0f7e1684d8d3da7492925
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2023-12-19 03:39:44 +0900

    [level unrated] Title: k의 개수, Time: 21.50 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.py
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.py
@@ -1,6 +1,7 @@
 def solution(i, j, k):
     answer = 0
-    for p in range(i,j+1):
-        if str(k) in str(p):
-            answer +=str(p).count(str(k))
+    for a in range(i,j+1):
+        for b in str(a):
+            if b == str(k):
+                answer +=1
     return answer
