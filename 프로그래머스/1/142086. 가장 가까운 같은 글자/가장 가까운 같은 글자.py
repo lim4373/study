3340a1fcaddaa6bf6ecc83fbe8d60e78e0edc258
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2023-12-20 23:14:37 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.81 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = []
+    s_dict = dict()
+    for i in range(len(s)):
+        if s[i] not in s_dict:
+            answer.append(-1)
+        else:
+            answer.append(i-s_dict[s[i]])
+        s_dict[s[i]] = i
+    return answer
