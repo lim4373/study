3aca72ca1bde8723f8f972683e86342cb51487ee
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-01-17 03:45:14 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,14 +1,19 @@
 def solution(number, limit, power):
-    divisors = []                            
-    for i in range(1, number+1):              
-        divisor = 0
-        for j in range(1, int(i**(1/2)) + 1): 
-            if i % j == 0:
-                divisor += 1
-                if j**2 != i:                 
-                    divisor += 1
-            if divisor > limit:              
-                divisor = power
+    num_list= []
+    for i in range(1,number+1):
+        num_1=0
+        for j in range(1,int(i**(0.5))+1):
+            if i%j==0:
+                num_1+=1
+                if j**2 !=i:
+                    num_1+=1
+            
+            if num_1>limit:
+                num_1=power
                 break
-        divisors.append(divisor)
-    return sum(divisors)
+        num_list.append(num_1)
+            
+    return sum(num_list)
+            
+                    
+                
