777c2bd967b1ed9367cb70d75aa7a872dfbeb9e1
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-02-23 02:47:21 +0900

    [level 1] Title: 소수 찾기, Time: 3257.56 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/README.md
+++ 프로그래머스/1/12921. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 찾기 - 12921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921?language=python3) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 2974.43 ms
+메모리: 10.1 MB, 시간: 3257.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 0:17:15
+2024년 02월 23일 02:47:17
 
 ### 문제 설명
 
--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,12 +1,16 @@
 import math
-def isprime(n):
-    for i in range(2,int(math.sqrt(n))+1):
-        if n % i == 0:
+
+def prime(x):
+    if x<=1:
+        return False
+    for i in range(2,int(math.sqrt(x))+1):
+        if x % i == 0:
             return False
-    return True   
+    return True
+
 def solution(n):
     answer = 0
-    for i in range(2,n+1):
-        if isprime(i) == True:
-            answer +=1
+    for num in range(2,n+1):
+        if prime(num):
+            answer+=1
     return answer
