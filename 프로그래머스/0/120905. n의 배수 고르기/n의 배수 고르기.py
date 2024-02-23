24b167decfb9ba812a4d2e7a0e1fbf750d8368bf
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-02-24 00:01:53 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,6 +1,4 @@
 def solution(n, numlist):
-    answer = []
-    for i in numlist:
-        if i % n == 0:
-            answer.append(i)
-    return answer
+    return [i for i in numlist if i % n ==0]
+
+
