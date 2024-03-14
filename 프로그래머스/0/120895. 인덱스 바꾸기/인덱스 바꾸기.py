773c85a1794588a5ee5c7ed449a49442dbd3c971
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-03-14 15:21:07 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,5 +1,4 @@
 def solution(my_string, num1, num2):
-    answer = ''
     my_string = list(my_string)
     my_string[num1],my_string[num2] = my_string[num2],my_string[num1]
-    return answer.join(my_string)
+    return ''.join(my_string)
