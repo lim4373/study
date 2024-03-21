37545281c466c9b64d50c3cd90921cfb6b3d8828
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-03-22 00:10:12 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    for i in my_string:
+        if i.isalpha():
+            my_string = my_string.replace(i, ' ')
+    my_string = my_string.split()
+    
+    return sum(list(map(int, my_string)))
