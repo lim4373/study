afc97a190f57db97fd613d16d1530910a8741bef
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-03-22 01:14:24 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.py
@@ -0,0 +1,9 @@
+def solution(array):
+    return ''.join(map(str,array)).count('7')
+
+# def solution(array):
+#     answer = ''
+#     for i in array:
+#         answer+=str(i)
+        
+#     return answer.count('7')
