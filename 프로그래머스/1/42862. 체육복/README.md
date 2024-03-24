d5b8df6b85fb1428c97c0470faf535ffe8056b34
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-03-25 00:29:17 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 7.81 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 7.81 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 22:49:18
+2024년 03월 25일 00:29:15
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,15 +1,15 @@
 def solution(n, lost, reserve):
     lost.sort()
     reserve.sort()
-    
     for i in reserve[:]:
         if i in lost:
             reserve.remove(i)
             lost.remove(i)
-    
+            
     for i in reserve:
         if i-1 in lost:
             lost.remove(i-1)
         elif i+1 in lost:
             lost.remove(i+1)
+            
     return n-len(lost)
