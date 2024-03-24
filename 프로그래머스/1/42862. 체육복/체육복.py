407cf78f23eefa6873c04adee6b610127675abee
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-03-25 01:59:55 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 7.84 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,17 +1,29 @@
 def solution(n, lost, reserve):
-    lost_set = set(lost)
-    reserve_set = set(reserve)
-    answer = n
+    answer = 0
+    lost.sort()
+    reserve.sort()
 
-    reserve_and_lost = lost_set & reserve_set
-    lost_set -= reserve_and_lost
-    reserve_set -= reserve_and_lost
+    for index_lost in range(len(lost)):
+        for index_reserve in range(len(reserve)):
+            if lost[index_lost] == reserve[index_reserve]:
+                lost[index_lost] = -2
+                reserve[index_reserve] = -2
 
-    for x in reserve_set:
-        if x-1 in lost_set:
-            lost_set.remove(x-1)
-        elif x+1 in lost_set:
-            lost_set.remove(x+1)
+    borrow = [False for _ in range(len(lost))]
+    for index_lost, lst in enumerate(lost):
+        if lost[index_lost] == -2:
+            borrow[index_lost] = True
+            continue
 
-    answer -= len(lost_set)
+        for index_res, res in enumerate(reserve):
+            if res == -2 or res == 0:
+                continue
+
+            if res == lst-1 or res == lst or res == lst+1:
+                borrow[index_lost] = True
+                reserve[index_res] = -2
+                break
+
+    answer = n - len([False for br in borrow if br is False])
+
     return answer
