b532d3b9a91447d551ac83b4e28709dc48cc8552
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-03-27 00:08:19 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -6,7 +6,7 @@ def solution(n):
                 answer +=i
                 
     elif  n%2 ==0:
-        for i in range(1,n+1):
+        for i in range(2,n+1):
             if i%2 ==0:
                 answer +=i**2
 
