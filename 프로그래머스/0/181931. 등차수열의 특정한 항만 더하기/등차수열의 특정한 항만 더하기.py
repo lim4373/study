5fd49ad591c955ef600f2f1ad6520fcbda95143e
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-04-07 00:35:38 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,5 +1,2 @@
 def solution(a, d, included):
-    answer = 0
-    for i in range(len(included)):
-        answer += (a+d*i)*int(included[i])
-    return answer
+    return sum((a+d*i)*int(included[i]) for i in range(len(included)))
