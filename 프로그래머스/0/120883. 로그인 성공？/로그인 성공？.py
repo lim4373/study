83b0ea2b27de96063ae6c382db33e3b2b9301226
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-04-09 14:16:21 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,7 @@
+def solution(id_pw, db):
+    for i in db:
+        if id_pw[0] in i:
+            if id_pw[1] == i[1]:
+                return "login"
+            return "wrong pw"
+    return "fail"
