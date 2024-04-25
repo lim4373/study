3f78e27ad185783b68584123025570e96cdb10fa
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-04-25 21:30:31 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,10 +1,6 @@
 def solution(n):
     answer = 0
-    sum = 0
-    
-    for i in range(1, n+1):
-        if n % i == 0:
-            sum += i
-            answer = sum
-    
+    for i in range(1,n+1):
+        if n % i ==0:
+            answer+=i
     return answer
