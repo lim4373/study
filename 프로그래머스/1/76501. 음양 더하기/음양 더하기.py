f3771d8b2a34e49cb120a12961125785f04ac640
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-04-26 19:55:27 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,8 @@
 def solution(absolutes, signs):
     answer = 0
-    for i in range(len(absolutes)): 
-        if signs[i]: 
+    for i in range(len(absolutes)):
+        if signs[i]:
             answer += absolutes[i]
         else:
-            answer -= absolutes[i]
+            answer -=absolutes[i]
     return answer
