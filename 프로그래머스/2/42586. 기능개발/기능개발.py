15eadee6e94d97ba3835b0074dddb47852902c57
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-04-29 00:53:35 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,16 @@
+def solution(progresses, speeds):
+    answer = []
+    days= 0
+    count = 0
+    while len(progresses)>0:
+        if (progresses[0]+days*speeds[0]) >=100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count+=1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            days+=1
+    answer.append(count)        
+    return answer
