709c2c90d67d14a4fe5212f822ee730010bbb0e2
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-04-29 00:02:11 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 105.71 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,6 +1,6 @@
 def solution(arr):
-    answer = []
-    for i in range(len(arr)):
-        if [arr[i]]!= arr[i+1:i+2]:
-            answer.append(arr[i])      
+    answer=[arr[0]]
+    for i in range(1,len(arr)):
+        if arr[i] !=arr[i-1]:
+            answer.append(arr[i])
     return answer
