d2ecb1a05e5e5868c6f92579fdf6cfa6ac23b1fc
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-02 00:00:59 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,9 +1,17 @@
+# def solution(number):
+#     answer = 0
+#     l = len(number)
+#     for i in range(l):
+#         for j in range(i+1,l):
+#             for k in range(j+1,l):
+#                 if number[i]+number[j]+number[k] == 0:
+#                     answer+=1
+#     return answer
+
+from itertools import combinations
 def solution(number):
-    a = len(number)
-    answer = 0
-    for i in range(len(number)):
-        for j in range(i+1,a):
-            for k in range(j+1,a):
-                if number[i]+number[j]+number[k] == 0:
-                    answer+=1
-    return answer
+    cnt = 0
+    for i in combinations(number,3):
+        if sum(i) == 0:
+            cnt +=1
+    return cnt
