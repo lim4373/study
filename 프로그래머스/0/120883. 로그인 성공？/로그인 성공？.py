2395195527157c5f9b0a6adfdff290023882b328
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-02 22:59:06 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,7 +1,9 @@
 def solution(id_pw, db):
-    for i in db:
-        if id_pw[0] in i:
-            if id_pw[1] == i[1]:
-                return "login"
+    db_dict = {i[0]: i[1] for i in db}
+    if id_pw[0] in db_dict:
+        if id_pw[1] == db_dict[id_pw[0]]:
+            return "login"
+        else:
             return "wrong pw"
-    return "fail"
+    else:
+        return "fail"
