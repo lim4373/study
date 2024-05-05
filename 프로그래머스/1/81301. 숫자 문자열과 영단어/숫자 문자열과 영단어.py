9d2a195ef0ce3e5c2be445699bce4768c43c7734
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-05 17:26:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,5 +1,5 @@
 def solution(s):
-    kakao = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
-    for key in kakao:
-        s= s.replace(key,kakao[key])
+    alpha = {0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+    for i in alpha.items():
+        s = s.replace(i[1],str(i[0]))
     return int(s)
