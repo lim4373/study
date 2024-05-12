35935a8ac3da5cb59001400c2a0194b95b62655d
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-12 16:50:43 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,5 +1,5 @@
 def solution(food):
-    temp = '' 
-    for i in range(1, len(food)):
-        temp += str(i) * (food[i]//2)
-    return temp + '0' + temp[::-1]
+    answer = ''
+    for i in range(1,len(food)):
+        answer +=str(i)*(food[i]//2)
+    return answer + '0'+answer[::-1]
