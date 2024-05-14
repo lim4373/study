8bbf68e76f5446560278a873fc8d5ab4d05adcc9
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-14 23:57:50 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,8 @@
 def solution(a, b, n):
     answer = 0
     while (n >= a):
-        remain_bottle = n % a
-        n = (n//a) * b 
-        answer += n 
-        n += remain_bottle 
+        s = n % a
+        n = (n //a) * b
+        answer +=n
+        n +=s
     return answer
