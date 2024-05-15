c62a3ea31a0ef4113348b746d8fa2a48a05760bb
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-15 21:50:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,8 +1,19 @@
 def solution(n, arr1, arr2):
     answer = []
+    arr1_bin = []
+    arr2_bin = []
     for i in range(n):
-        num = bin(arr1[i] | arr2[i])
-        num = num[2:].zfill(n)
-        num = num.replace('1', '#').replace('0', ' ')
-        answer.append(num)
+        arr1_bin.append(bin(arr1[i])[2:])
+        arr2_bin.append(bin(arr2[i])[2:])
+        arr1_bin[i] = ('0' * (n-len(arr1_bin[i]))) + arr1_bin[i]
+        arr2_bin[i] = ('0' * (n-len(arr2_bin[i]))) + arr2_bin[i]
+    
+        tmp = ''
+        for p in range(n):
+            if arr1_bin[i][p] == '1' or arr2_bin[i][p] == '1':
+                tmp += '#'
+            elif arr1_bin[i][p] == '0' and arr2_bin[i][p] == '0':
+                tmp += ' '
+        answer.append(tmp)
+        
     return answer
