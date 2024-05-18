16470f249b38c3ec719fd7ee4292b4b3fe095924
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-18 11:43:35 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,6 +1,10 @@
+# def solution(n):
+#     answer =0
+#     for i in str(n):
+#         answer +=int(i)
+#     return answer
+
+
+
 def solution(n):
-    answer =0
-    for i in str(n):
-        answer +=int(i)
-    return answer
-        
+    return sum(map(int, str(n)))
