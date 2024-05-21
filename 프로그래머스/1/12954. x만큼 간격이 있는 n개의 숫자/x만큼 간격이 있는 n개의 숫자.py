416ba96c2e4660e51960816fc1035caaa4e60f23
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-21 22:30:03 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -1,3 +1,8 @@
-def solution(x, n):
-    return [x*i for i in range(1,n+1)]    
-    
+# def solution(x, n):
+#     answer = []
+#     for i in range(1,n+1):
+#         answer.append(x*i)
+#     return answer
+
+def solution(x,n):
+    return [x*i for i in range(1,n+1)]
