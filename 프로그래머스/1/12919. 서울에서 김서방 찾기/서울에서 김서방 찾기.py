2bf33812367ce80a5e5f659a9f002de1258cdf00
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-21 23:35:58 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,3 +1,8 @@
+# def solution(seoul):
+#     answer =''
+#     answer = "김서방은 {}에 있다".format(seoul.index("Kim"))
+#     return answer
+
 def solution(seoul):
     answer = seoul.index("Kim")
     return f"김서방은 {answer}에 있다"
