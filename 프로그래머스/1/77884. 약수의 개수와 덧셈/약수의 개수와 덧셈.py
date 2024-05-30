4757365c4d629b457cbb8402bda6f1e830e003d4
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-30 18:20:08 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.41 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,12 +1,13 @@
-import math
-
 def solution(left, right):
     answer = 0
-    for i in range(left, right + 1):
-        sqrt = math.sqrt(i)
-        if int(sqrt) == sqrt:
-            answer -= i
+    for i in range(left,right+1):
+        count = 0
+        for j in range(1,i+1):
+            if i % j ==0:
+                count +=1
+        if count % 2 == 0:
+            answer+=i
         else:
-            answer += i
-
+            answer-=i
+            
     return answer
