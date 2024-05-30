0d4982667d8dbab8b3c4d835c9ddb013693d876a
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-05-30 18:42:18 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,7 +1,12 @@
+# def solution(s):
+#     return s.isdigit() and len(s) in (4,6)
+
 def solution(s):
-    if len(s)==4 or len(s)==6:
-        if s.isdigit() == True:
+    if len(s) == 4 or len(s) ==6:
+        if s.isdigit()==True:
             return True
         else:
             return False
-    return False
+    else:
+        return False
+            
