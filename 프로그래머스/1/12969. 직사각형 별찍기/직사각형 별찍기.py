b662ceabc7443b44aba84653758fd9bbd81c8091
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-06-09 16:54:53 +0900

    [level 1] Title: 직사각형 별찍기, Time: 11.46 ms, Memory: 7.46 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.py
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.py
@@ -1,5 +1,8 @@
 a, b = map(int, input().strip().split(' '))
-for i in range(b):
-    for j in range(a):
-        print('*',end='')
-    print()
+# for i in range(b):
+#     for j in range(a):
+#         print("*", end='')
+#     print()
+
+answer = (a*'*'+'\n')*b
+print(answer)
