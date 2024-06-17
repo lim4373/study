05bca4f61322a6805126fa31b995f6cd571c5e46
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-06-17 23:08:39 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,7 +1,9 @@
-def gcd(n,m):
-    if n % m == 0:
+def gcd(n, m):
+    if n%m == 0:
         return m
     else:
-        return gcd(m,n%m)
-def solution(n,m):
-    return [gcd(m,n), n*m//gcd(m,n)]
+        return gcd(m, n%m)
+    
+def solution(n, m):
+    answer = [gcd(m, n), n*m // gcd(m,n)]
+    return answer
