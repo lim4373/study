8e5ede5bbe2a53a8d5064087ef053fa880ad0990
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-10-14 22:58:32 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,2 +1,8 @@
 def solution(date1, date2):
-    return 1 if int("".join(map(str,date1))) < int("".join(map(str,date2))) else 0
+    answer = "".join(map(str,date1))
+    answer1 = "".join(map(str,date2))
+    
+    if int(answer) < int(answer1):
+        return 1
+    return 0 
+   
