d3b1753ab9eae0b911688eaa550f3b6726a29690
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-10-15 23:05:07 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -1,9 +1,14 @@
 def solution(n, slicer, num_list):
-    a,b,c = slicer
-    if n==1:
+    if n == 1:
+        a,b = slicer[0],slicer[1]
         return num_list[:b+1]
-    elif n==2:
+    elif n == 2:
+        a,b = slicer[0],slicer[1]
         return num_list[a:]
-    elif n==3:
+    elif n ==3:
+        a,b = slicer[0],slicer[1]
         return num_list[a:b+1]
-    return num_list[a:b+1:c]
+    elif n==4:
+        a,b,c = slicer[0],slicer[1],slicer[2]
+        return num_list[a:b+1:c]
+        
