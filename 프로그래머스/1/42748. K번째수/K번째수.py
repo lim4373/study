05cd56bed456890cf78ec3835dd94ca0341110fc
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2024-11-14 18:01:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,6 @@
 def solution(array, commands):
     answer = []
-    for i in range(len(commands)):
-        arr = array[commands[i][0]-1:commands[i][1]]
-        arr.sort()
-        answer.append(arr[commands[i][2]-1])
+    for i,j,k in commands:
+        a = sorted(array[i-1:j])
+        answer.append(a[k-1])
     return answer
