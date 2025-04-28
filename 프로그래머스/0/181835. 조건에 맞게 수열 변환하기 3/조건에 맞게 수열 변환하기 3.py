d2913930613ebb6d25f47fa1226f703044a9c131
SungLim Choi <114973162+lim4373@users.noreply.github.com>
2025-04-28 21:24:51 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 109.56 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -1,8 +1,18 @@
-def solution(arr, k):
-    answer = []
+def solution(arr,k):
+    answer=[]
     for i in arr:
-        if k % 2 == 0:
-            answer.append(i+k)
-        else:
+        if k % 2 == 1:
             answer.append(i*k)
+        else:
+            answer.append(i+k)
     return answer
+            
+
+
+# def solution(arr, k):
+#     if k % 2 !=0:
+#         return list(map(lambda x:x*k,arr))
+#     return list(map(lambda x:x+k,arr))
+    
+
+
